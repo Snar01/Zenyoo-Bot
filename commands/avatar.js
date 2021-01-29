3f998b733c777813c33cbabfbf637605624763d6
Leonardo-ST <70207858+Leonardo-ST@users.noreply.github.com>
2021-01-29 07:13:20 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,16 +1,17 @@
 const Discord = require("discord.js");
 
-exports.run = async (client, message, args) => {
+module.exports.run = async (client, message, args) => {
 
   let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
 
   let avatar = user.avatarURL({ dynamic: true, format: "png", size: 1024 });
 
-  let embed = new Discord.MessageEmbed()
-    .setColor(`#4cd8b2`)
+  const foto = new Discord.MessageEmbed()
+    .setColor(`RANDOM`)
     .setTitle(`Avatar de ${user.username}`)
+    .setDescription(`[Clique aqui](${avatar}) para baixar.`)
     .setImage(avatar)
     .setFooter(`• Autor: ${message.author.tag}`, message.author.displayAvatarURL({ format: "png" }));
-  await message.channel.send(embed);
+  await message.channel.send(foto);
 
 };
