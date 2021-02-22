1b76a5e2b4b61f83834a7b4d1789202deb3a23ea
Boss1260 <74876502+Boss1260@users.noreply.github.com>
2021-02-22 13:21:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,113 +1,88 @@
 const express = require('express');
 const app = express();
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
   const ping = new Date();
   ping.setHours(ping.getHours() - 3);
-  console.log(
-    `Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
-  );
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
   response.sendStatus(200);
 });
-app.listen(process.env.PORT); // Recebe solicitações que o deixa online
+app.listen(process.env.PORT);
 
-const Discord = require('discord.js'); //Conexão com a livraria Discord.js
-const client = new Discord.Client(); //Criação de um novo Client
-const config = require('./config.json'); //Pegando o prefixo do bot para respostas de comandos
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
 
-client.on('message', message => {
+const fs = require("fs");
+const env = require("dotenv");
+env.config();
+
+//handler do bot
+client.config = config;
+
+client.commands = new Discord.Collection();
+client.aliases = new Map();
+client.queue = new Map();
+
+fs.readdir("./admin/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./admin/${file}`);
+    let commandName = props.help.name;
+    console.log(`Comando -----> ${commandName} <----- foi carregado na categoria admin`);
+    client.commands.set(commandName, props);
+    for (let i in props.help.analise) {
+      client.aliases.set(props.help.analise[i], commandName)
+    }
+  });
+});
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = props.help.name;
+    console.log(`Comando -----> ${commandName} <----- foi carregado na categoria gerais`);
+    client.commands.set(commandName, props);
+    for (let i in props.help.analise) {
+      client.aliases.set(props.help.analise[i], commandName)
+    }
+  });
+}); //este arquivo depois de fazer tudo, no final vao ter de adicionar o `exports.help = {name: "nome", analise: ["outra opeção de fazer"]}` mas não precisão de colocar o analise, se tirarem tem de tirar a virgula depois do nome
+/*
+//eventos
+fs.readdir('./events/', (err, files) => {
+  if (err) return console.error;
+  files.forEach(file => {
+      if (!file.endsWith('.js')) return;
+      const evt = require(`./events/${file}`);
+      let evtName = file.split('.')[0];
+      console.log(`Evento Carregado >>> '${evtName}'`);
+      client.on(evtName, evt.bind(null, client));
+  });
+});*/
+
+client.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type == 'dm') return;
-  if (!message.content.toLowerCase().startsWith(config.prefix)) return;
-  if (
-    message.content.startsWith(`<@!${client.user.id}>`) ||
-    message.content.startsWith(`<@${client.user.id}>`)
-  )
-    return;
 
-  const args = message.content
-    .trim()
-    .slice(config.prefix.length)
-    .split(/ +/g);
+  if (message.content.indexOf(client.config.prefix) !== 0) return;
+
+  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  try {
-    const commandFile = require(`./commands/${command}.js`);
-    commandFile.run(client, message, args);
-  } catch (err) {
-    console.error('Erro:' + err);
+  let cmd = client.commands.get(command);
+  if (!cmd) {
+    if (!client.aliases.get(command)) return
+    else cmd = client.commands.get(client.aliases.get(command));
   }
+  cmd.run(client, message, args);
 });
 
-//Aqui o Zenyoo responde quando alguém marca ele!
-client.on('message', async message => {
-  if (message.author.bot) return;
-  if (message.channel.type == 'dm') return;
-  let member = message.mentions.members.first();
-  if (member)
-    if (member.id == `${client.user.id}`)
-      message.channel.send(
-        'Olá, eu sou Zenyoo, se você me marcou é porquê quer saber meus comandos correto? Se for isso siga o quê eu digo abaixo.\n\nUse z!help para saber os meus comandos.'
-      );
+client.on("ready", () => {
+  console.log(`Estou ligado`);
+  client.user.setActivity(`OS ESTADOS DO BOT`)
 });
-
-//Aqui sempre que ele liga mostra no conole os servidores!
-client.on('ready', () => {
-  client.guilds.cache.map(x => {
-    console.log(`Nome: ${x.name}`);
-  });
-});
-
-//Aqui mostra no console os novos servidores!
-client.on('guildCreate', guild =>
-  console.log(
-    `Fui adicionado no servidor ${guild.name} (${guild.id}), dono: ${
-    guild.owner.user.tag
-    }`
-  )
-);
-
-//Aqui mostra no console quando ele sai de um servidor!
-client.on('guildDelete', guild =>
-  console.log(
-    `o garaio me tiraro do servidor ${guild.name}, to tristão meu, bane o ${
-    guild.owner.user.tag
-    }`
-  )
-);
-
-//Evento de status do bot
-client.on('ready', () => {
-  let activities = [
-    `Utilize ${config.prefix}help para saber quais os meus comandos`,
-    `Eu estou em ${client.guilds.cache.size} lugares diferentes!`,
-    `Use z!vote e me ajude a melhorar!`,
-    `com ${client.users.cache.size} amigos!`,
-    `Use z!suporte e entre para o meu server`,
-    `Fui criado por Leonardo ST#1295`
-  ],
-    i = 0;
-  setInterval(
-    () =>
-      client.user.setActivity(`${activities[i++ % activities.length]}`, {
-        type: 'PLAYING'
-      }),
-    12000
-  );
-  client.user.setStatus('online').catch(console.error);
-  console.log('Estou Online!');
-});
-
-//essa const so serve se seu bot estiver na Zulaaa.com
-const votosZuraaa = require('./votosZuraaa.js');
-
-// Evento de mensagens do bot, onde ele manda na dm agradecendo pelo voto
-client.on('message', message => {
-  votosZuraaa.verificaVotos(message, user => {
-    user.send(
-      'Obrigado por votar em mim, seu voto me ajuda a melhorar a cada dia. 😀'
-    );
-  });
-});
-
 
 client.login(process.env.TOKEN);
