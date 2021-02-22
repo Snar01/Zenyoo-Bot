a9646869b96391c5cdf94372e000bc06611ed5a3
Boss1260 <74876502+Boss1260@users.noreply.github.com>
2021-02-22 13:15:38 +0000

    Delete commands directory

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (client, message, args) => {
-  const m = await message.channel.send('ping?');
-
-  m.edit(`🏓 **| Pong!**\nMinha latência no Server: **${m.createdTimestamp -
-    message.createdTimestamp}ms.**\nLatência da API: **${Math.round(
-      client.ws.ping
-    )}ms**`
-  );
-};
